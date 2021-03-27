4c0b596c463b2ab16daca8acc6912d69ab947fb7
Izak <imitatio-ignotum@protonmail.com>
2021-03-27 20:42:43 +0000

    Rewrite core.s
    * Changed all ".word" psuedo-ops to ".int".
    * Removed the "intro" word.
    * Added the "accept" word.
    * Added the "word" word.

--- core.s
+++ core.s
@@ -1,18 +1,31 @@
-
-	/* Linux file descriptors */
+/* Linux file stream descriptors */
 	.set stdin, 0
 	.set stdout, 1
 	.set stderr, 2
 
-	/* System call numbers */
+/* System call numbers */
 	.set os_exit, 1
 	.set os_read, 3
 	.set os_write, 4
 
-	/* Word bitmask flags */
+/* Word bitmask flags */
 	.set F_IMMEDIATE, 0b10000000
 	.set F_HIDDEN,    0b01000000
 	.set F_LENMASK,   0b00111111    
+
+/* Macro for defining a word header in the data section */
+	.macro define name, len, flags=0, label   
+	.data
+	.align 2
+_def_\label:
+	.int link               // link to previous word in the dictionary data
+	.set link, _def_\label
+	.byte \len+\flags        // The name field, including the length byte
+	.ascii "\name"           // is 32 bytes long
+	.space 31-\len
+	.global xt_\label
+xt_\label:                   // The next 4 byte word/cell will be the code field
+	.endm
 
 /* Begin normal program data, which needs to be before the dictionary
  * because the dictionary will grow upwards in memory.
@@ -22,10 +35,8 @@
 
 data_base:
 
-	/* The stacks */
-	.space 4*64     /* 64 cells for parameter stack, which grows down */
-stack_base:
-	.space 4*256    /* 256 cells for the return stack, which grows up */
+	.space 4*256    /* 256 cells for the return stack, which grows down */
+rstack_base:
 
 	/* Terminal Input Buffer */
 addr_tib:
@@ -36,274 +47,252 @@ addr_emit_char:
 
 /* Begin word header definitions */
 
+	.set link, 0   // Previous word link pointer for while it's assembling word defs
+
 	.data
-
-	/* Previous word link pointer for while it's assembling word defs */
-	.set link, 0
-
-	/* Macro for defining a word header */
-	.macro define name, len, flags=0, label   
-	.data
-	.align 2
-_def_\label:
-	.word link
-	.set link, _def_\label
-	.byte \len+\flags
-	.ascii "\name"
-	.space 31-\len
-	.align 2
-	.global xt_\label
-xt_\label: 
-	// The next 4 byte word/cell is the code field
-	.endm
-
 dictionary_base:
 
 	/* : ( -- ) */
 	define ":", 1, F_IMMEDIATE, colon
-	.word docol
-	.word xt_create                // Create a new header for the next word.
-	.word xt_enter_compile         // Enter into compile mode
-	.word xt_do_semi_code, docol   // Make "docolon" be the runtime code for the new header.
+	.int docol
+	.int xt_create                        // Create a new header for the next word.
+	.int xt_enter_compile                 // Enter into compile mode
+	.int xt_lit, docol, xt_do_semi_code   // Make "docolon" be the runtime code for the new header.
 
 	define "]", 1, , enter_compile
-	.word enter_compile
+	.int enter_compile
 
 	define "[", 1, F_IMMEDIATE, enter_immediate
-	.word enter_immediate
+	.int enter_immediate
 
 	define "quit", 4, , quit
-	.word quit
+	.int quit
 
 	define "state", 5, , state
-	.word dovar
+	.int dovar
 var_state:
-	.word 0
+	.int 0
 
 	define ">in", 3, , to_in
-	.word dovar
+	.int dovar
 var_to_in:
-	.word 0
+	.int 0
 
 	define "#tib", 4, , num_tib
-	.word dovar
+	.int dovar
 var_num_tib:
-	.word 0
+	.int 0
 
 	define "dp", 2, , dp
-	.word dovar
+	.int dovar
 var_dp:
-	.word 0
+	.int 0
 
 	define "base", 4, , base
-	.word dovar
+	.int dovar
 var_base:
-	.word 10
+	.int 10
 	
 	define "last", 4, , last
-	.word dovar
+	.int dovar
 var_last:
-	.word the_final_word
+	.int the_final_word
 
 	define "tib", 3, , tib
-	.word doconst
-	.word addr_tib
+	.int doconst
+	.int addr_tib
 
 	define ";", 1, F_IMMEDIATE, semicolon
-	.word docol
-	.word xt_lit, xt_exit                // Compile an exit code.
-	.word xt_comma
-	.word xt_lit, 0, xt_state, xt_store  // Change back to immediate mode.
-	.word xt_exit                        // Actually exit this word.
+	.int docol
+	.int xt_lit, xt_exit                // Compile an exit code.
+	.int xt_comma
+	.int xt_lit, 0, xt_state, xt_store  // Change back to immediate mode.
+	.int xt_exit                        // Actually exit this word.
 
 	define "create", 6, , create
-	.word docol
-	.word xt_dp, xt_fetch
-	.word xt_last, xt_fetch
-	.word xt_comma
-	.word xt_last, xt_store
-	.word xt_lit, 32
-	.word xt_word
-	.word xt_count
-	.word xt_add
-	.word xt_dp, xt_store
-	.word xt_lit, 0
-	.word xt_comma
-	.word xt_do_semi_code, dovar
+	.int docol
+	.int xt_dp, xt_fetch
+	.int xt_last, xt_fetch
+	.int xt_comma
+	.int xt_last, xt_store
+	.int xt_lit, 32
+	.int xt_word
+	.int xt_count
+	.int xt_add
+	.int xt_dp, xt_store
+	.int xt_lit, 0
+	.int xt_comma
+	.int xt_lit, dovar, xt_do_semi_code
 
 	define "(;code)", 7, , do_semi_code
-	.word do_semi_code
+	.int do_semi_code
 
 	define "const", 5, , const
-	.word docol
-	.word xt_create
-	.word xt_comma
-	.word xt_do_semi_code, doconst
+	.int docol
+	.int xt_create
+	.int xt_comma
+	.int xt_lit, doconst, xt_do_semi_code
 
 	define "lit", 3, F_IMMEDIATE, lit
-	.word lit
+	.int lit
 
 	define ",", 1, , comma
-	.word comma
+	.int comma
 
 	define "drop", 4, , drop
-	.word drop
+	.int drop
 
 	define "swap", 4, , swap
-	.word swap
+	.int swap
 
 	define "over", 4, , over
-	.word over
+	.int over
 
 	define "rot", 3, , rot
-	.word rot
+	.int rot
 
 	define ">R", 2, , to_r
-	.word to_r
+	.int to_r
 
 	define "R>", 2, , r_from
-	.word r_from
+	.int r_from
 
 	define "-", 1, , sub
-	.word sub
+	.int sub
 
 	define "+", 1, , add
-	.word add
+	.int add
 
 	define "dup", 3, , dup
-	.word dup
+	.int dup
 
 	define "find", 4, , find
-	.word find
+	.int find
 
 	define "emit", 4, , emit
-	.word emit
+	.int emit
 
 	define "=", 1, , equal
-	.word equal
+	.int equal
 
 	define "*", 1, , multiply
-	.word multiply
+	.int multiply
 
 	define "<", 1, , lt
-	.word lt
+	.int lt
 
 	define ">", 1, , gt
-	.word gt
+	.int gt
 
 	define "&", 1, , and
-	.word do_and
+	.int do_and
 
 	define "|", 1, , or
-	.word do_or
+	.int do_or
 
 	define "^", 1, , xor
-	.word xor
+	.int xor
 
 	define "invert", 6, , invert
-	.word invert
+	.int invert
 
 	define "!", 1, , store
-	.word store
+	.int store
 
 	define "@", 1, , fetch
-	.word fetch
+	.int fetch
 
 	define "c!", 2, , cstore
-	.word cstore
+	.int cstore
 
 	define "c@", 2, , cfetch
-	.word cfetch
+	.int cfetch
 
 	define "exit", 4, , exit
-	.word exit
+	.int exit
 
 	define "branch", 6, , branch
-	.word branch
+	.int branch
 
 	define "0branch", 7, , zero_branch
-	.word zero_branch
+	.int zero_branch
 
 	define "exec", 4, , exec
-	.word exec
+	.int exec
 
 	// count ( addr -- addr2 len )
 	define "count", 5, , count
-	.word count
+	.int count
 
 	// tell ( addr -- )
 	define "tell", 4, , tell
-	.word tell
+	.int tell
 
 	define ">number", 7, , to_number
-	.word to_number
+	.int to_number
 
 	define "accept", 6, , accept
-	.word accept
+	.int accept
 
 	define "word", 4, , word
-	.word word
+	.int word
 
 	define "interpret", 9, , interpret
-	.word docol
-	.word xt_intro
-	.word xt_num_tib
-	.word xt_fetch
-	.word xt_to_in
-	.word xt_fetch
-	.word xt_equal
-	.word xt_zero_branch, intpar
-	.word xt_tib
-	.word xt_lit, 50
-	.word xt_accept
-	.word xt_num_tib
-	.word xt_store
-	.word xt_lit, 0
-	.word xt_to_in
-	.word xt_store
+	.int docol
+	.int xt_num_tib
+	.int xt_fetch
+	.int xt_to_in
+	.int xt_fetch
+	.int xt_equal
+	.int xt_zero_branch, intpar
+	.int xt_tib
+	.int xt_lit, 50
+	.int xt_accept
+	.int xt_num_tib
+	.int xt_store
+	.int xt_lit, 0
+	.int xt_to_in
+	.int xt_store
 intpar:
-	.word xt_lit, 32
-	.word xt_word
-	.word xt_find
-	.word xt_dup
-	.word xt_zero_branch, intnf
-	.word xt_state
-	.word xt_fetch
-	.word xt_equal
-	.word xt_zero_branch, intexc
-	.word xt_comma
-	.word xt_branch, intdone
+	.int xt_lit, 32
+	.int xt_word
+	.int xt_find
+	.int xt_dup
+	.int xt_zero_branch, intnf
+	.int xt_state
+	.int xt_fetch
+	.int xt_equal
+	.int xt_zero_branch, intexc
+	.int xt_comma
+	.int xt_branch, intdone
 intexc:
-	.word xt_exec
-	.word xt_branch, intdone
+	.int xt_exec
+	.int xt_branch, intdone
 intnf:
-	.word xt_dup
-	.word xt_rot
-	.word xt_count
-	.word xt_to_number
-	.word xt_zero_branch, intskip
-	.word xt_state, xt_fetch
-	.word xt_zero_branch, intnc
-	.word xt_last, xt_fetch
-	.word xt_dup, xt_fetch
-	.word xt_last, xt_store
-	.word xt_dp, xt_store
+	.int xt_dup
+	.int xt_rot
+	.int xt_count
+	.int xt_to_number
+	.int xt_zero_branch, intskip
+	.int xt_state, xt_fetch
+	.int xt_zero_branch, intnc
+	.int xt_last, xt_fetch
+	.int xt_dup, xt_fetch
+	.int xt_last, xt_store
+	.int xt_dp, xt_store
 intnc:                                       // Exit infinite loop and reset
-	.word xt_quit                            // because of error.
+	.int xt_quit                            // because of error.
 intskip:
-	.word xt_drop, xt_drop
-	.word xt_state, xt_fetch
-	.word xt_zero_branch, intdone
-	.word xt_lit, xt_lit, xt_comma, xt_comma
+	.int xt_drop, xt_drop
+	.int xt_state, xt_fetch
+	.int xt_zero_branch, intdone
+	.int xt_lit, xt_lit, xt_comma, xt_comma
 intdone:
-	.word xt_branch, xt_interpret            // Infinite loop.
-
-	define "intro", 3, F_HIDDEN, intro
-	.word _intro
+	.int xt_branch, xt_interpret            // Infinite loop.
 
 the_final_word:
 	define "bye", 3, , bye
-	.word exit_program
+	.int exit_program
 
 /* Begin the main assembly code. */
 
@@ -311,15 +300,38 @@ the_final_word:
 
 	/* Main starting point. */
 	.global _start
-_start:                 
-	b quit
-	
-next:                    // The inner interpreter
-	ldr r8, [r10], #4    // Get the next virtual instruction
-	bx r8
+_start:
+
+    // fall-through to quit
+
+quit:
+	ldr r11, =rstack_base    // Init the return stack.
+
+	ldr r1, =var_state      // Set state to 0 (interpreting)
+	eor r0, r0
+	str r0, [r1]
+
+	ldr r0, =var_num_tib      // Copy value of "#tib" to ">in".
+	ldr r0, [r0]
+	ldr r1, =var_to_in
+	str r0, [r1]
+
+	ldr r10, =xt_interpret    // Set instruction pointer to interpret
+	b next  
 
 docol:
-	str r10, [r11, #4]!    // Push the (next) IP to the stack
+	str r10, [r11, #-4]!    // Save the return address to the return stack
+	add r10, r8, #4        // Get the next instruction
+
+	// fall-into next
+
+next:                    // The inner interpreter
+	ldr r8, [r10], #4    // Get the next virtual instruction
+	ldr r1, [r8]
+	bx r1
+
+exit:                     // End a forth word.
+	ldr r10, [r11], #4    // ip = pop return stack
 	b next
 
 dovar:
@@ -333,26 +345,12 @@ doconst:                   // Runtime code for words that push a constant.
 	ldr r9, [r8, #4]       // Fetch the data, which is bytes 4 after the CFA.
 	b next                 
 
-do_semi_code:
-	ldr r8, =var_last
-	add r8, #36           // Offset to Code Field Address.
-	str r10, [r8]         // Store Instruction Pointer into the Code Field.
-	// TODO
-
-quit:
-	ldr r11, =stack_base    // Init the return stack.
-	ldr sp, =stack_base     // Init the data stack.
-
-	ldr r1, =var_state      // Set state to 0.
-	eor r0, r0
-	str r0, [r1]
-
-	ldr r0, =var_num_tib      // Copy value of "#tib" to ">in".
+do_semi_code:             // (;code) - ( addr -- ) replace the xt of the word being defined with addr
+	ldr r0, =var_last     // Get the latest word.
 	ldr r0, [r0]
-	ldr r1, =var_to_in
-	str r0, [r1]
-
-	ldr r10, =xt_interpret
+	add r0, #36           // Offset to Code Field Address.
+	str r9, [r0]          // Store the code address into the Code Field.
+	pop {r9}
 	b next
 
 exit_program:
@@ -374,6 +372,7 @@ comma:
 	b next
 
 find:
+	// TODO
 	b next
 
 drop:
@@ -492,10 +491,6 @@ cfetch:
 	ldr r9, [r13], #4
 	b next 
 
-exit:
-	ldr r10, [r11], #4   // ip = pop return stack
-	b next
-
 branch:
 	ldr r1, [r10]
 	mov r10, r1    // absolute jump
@@ -510,10 +505,10 @@ zero_branch:
 	b next
 
 exec:
-	mov r0, r9        // save TOS to r0
+	mov r8, r9        // save TOS to r0
 	ldr r9, [r13], #4 // pop the stack
-	ldr r0, [r0]      // dereference r0
-	bx r0             // goto r0
+	ldr r1, [r8]      // dereference r0
+	bx r1             // goto r0
 
 count:
 	mov r0, r9          // push addr + 1
@@ -587,13 +582,68 @@ to_num5:
 	str r0, [r13, #-4]!  // push the string address
 	b next
 
-accept:
-	// TODO
+accept:                   // accept - ( addr len -- len2 )
+	pop {r1}              // r1 = buffer address.
+
+	mov r0, #stdin
+	mov r2, r9            // r2 = count
+	mov r7, #os_read
+	swi #0
+
+	cmp r0, #-1           // read(...) returns -1 upon an error.
+	beq exit_program
+
+	mov r9, r0
 	b next
 
-word:
-	// TODO
-	b next
+word:                     // word - ( char -- addr )
+	mov r0, r9            // r0 = char
+	ldr r9, =var_dp       // push the dictionary pointer, which is used as a buffer area, "pad"
+	ldr r9, [r9]
+	mov r4, r9
+
+	ldr r1, =var_tib      // r1 = tib
+	ldr r1, [r1]
+	mov r2, r1            // r2 = tib
+
+	ldr r3, =var_to_in    // r1 += >in, r1 = pointer into the buffer
+	ldr r3, [r3]
+	add r1, r3
+
+	ldr r3, =var_num_tib  // r2 += #tib, r2 = last char address in buffer
+	ldr r3, [r3]
+	add r2, r3            
+word_skip_skip:
+	cmp r1, r2
+	beq word_done
+
+	ldrb r3, [r1], #1     // get next char
+	cmp r0, r3
+	beq word_skip
+word_copy:
+	add r4, #1
+	strb r3, [r4]
+
+	cmp r1, r2
+	beq word_done
+	
+	ldrb r3, [r1], #1     // get next char
+	cmp r0, r3
+	bne word_copy
+word_done:
+	mov r3, #' '          // write a space to the end of the pad
+	str r3, [r4, #1]
+	sub r4, r9            // get the length
+	strb r4, [r9]         // store the length byte into the first char of the pad
+
+	ldr r3, =var_tib      // Get new value of >in and save it to its variable
+	ldr r3, r3
+	sub r2, r3
+	ldr r3, =var_to_in    
+	ldr r3, r3
+	str r2, [r3]
+
+	b next                // TOS (r9) has been pointing to the pad addr the whole time
 
 emit:
 	pop {r3}
@@ -616,16 +666,4 @@ enter_immediate:             // Exit compile mode and enter immediate mode.
 	eor r1, r1               // false = 0 = not compiling
 	str r1, [r0]
 	b next
-
-_intro:
-	push {r9}
-	ldr r9, =_intro_str
-	b tell
-
-	.section .rodata
-_intro_str:
-	.byte _intro_msg_end-_intro_msg
-_intro_msg:
-	.ascii "Intro\n"
-_intro_msg_end:
 
