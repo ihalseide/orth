ad67abd377a0071bc47551ed9bacb78f92da53d1
ihalseide <imitatio-ignotum@protonmail.com>
2021-04-02 22:19:52 -0600

    Markup the interpreter code

--- interpreter.fs
+++ interpreter.fs
@@ -3,18 +3,22 @@
 : cs>number ( ca1 -- ud ca2 len ) 0 0 rot >number ;
 
 : do-compile ( -- / x*i -- x*j )
-	find-word?  dup if
-		1 = if
-				execute
+	find-word?  dup
+	if
+		( 1 ) 1 =
+		if
+			( 2 ) execute
 		else
-				,
+			( 3 ),
 		then
 	else
-		drop cs>number if
-		last @ dup @ last ! dp ! abort
-	then
-		drop drop ['] lit , ,
-	then ;
+		( 4 ) drop cs>number
+		if
+			( 5 ) last @ dup @ last ! dp ! abort
+		then
+		( 6 ) drop drop ['] lit , ,
+	then 
+	( 7 ) ;
 
 : do-interpret ( --n / x*i -- x*j )
 	find-word? dup if
