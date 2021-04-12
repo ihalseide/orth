6a1c18f8248007e44e1a753c34e3cf97844391b3
ihalseide <imitatio-ignotum@protonmail.com>
2021-04-12 15:16:46 -0600

    Work on u_dot code

--- u_dot.py
+++ u_dot.py
@@ -11,50 +11,48 @@ def u_dot (n, base=10):
         print(end='0')
         return
 
-    i = 0
     num_buf = [0] * 33
-
-    num_buf[0] = ord('0')
+    i = -1
 
     # Extract digits based on the number base
     if base == 2:
         while n:
-            num_buf[i] = n & 0b1
             i += 1
+            num_buf[i] = n & 0b1
             n = n >> 1
     elif base == 4:
         while n:
-            num_buf[i] = n & 0b11
             i += 1
+            num_buf[i] = n & 0b11
             n = n >> 2
     elif base == 8:
         while n:
-            num_buf[i] = n & 0b111
             i += 1
+            num_buf[i] = n & 0b111
             n = n >> 3
     elif base == 16:
         while n:
-            num_buf[i] = n & 0b1111
             i += 1
+            num_buf[i] = n & 0b1111
             n = n >> 4
     elif base == 32:
         while n:
-            num_buf[i] = n & 0b11111
             i += 1
+            num_buf[i] = n & 0b11111
             n = n >> 5
     else:
         while n:
             q, r = divmod(n, base)
-            num_buf[i] = r
             i += 1
+            num_buf[i] = r
             n = q
 
     # Print the number buffer
-    for j in range(i-1, -1, -1):
+    for j in range(i, -1, -1):
         c = num_buf[j]
         if c > 9:
             c += ord('A') - ord('9') - 1
-        c = chr(ord('0')+c)
+        c = chr(ord('0') + c)
         print(end=c)
 
 base = 16
---
+++ u_dot.s
@@ -0,0 +1,118 @@
+// FORTH word
+// u>chars ( u -- addr n )
+// where u is an unsigned number
+// where addr is the pad address, for the beginning address of the character string
+// where n is the length of the string of chars, n is 0 if error
+u_dot:
+	ldr r0, =var_pad   // r0 = pad addr
+	ldr r0, [r0]
+	push {r0}          // push addr
+
+	ldr r1, =var_base  // r1 = number base
+	ldr r1, [r1]
+
+	cmp r1, #2         // if base < 2, error condition
+	movlt r9, #0          
+	blt next
+
+	cmp r9, #0         // handle the trivial case of u = 0
+	moveq r1, #'0'
+	streq r1, [r0]
+	moveq r9, #1
+	beq next
+
+	mov r2, #-1        // r2 = index i
+
+    // Jump to extract digits with a base that is a power of 2
+	cmp r1, #2
+	beq base2
+	cmp r1, #4
+	beq base4
+	cmp r1, #8
+	beq base8
+	cmp r1, #16
+	beq base16
+	cmp r1, #32
+	beq base32
+
+baseN:                        // Base that is not a power of 2
+	cmp r9, #0
+	beq finish
+
+	                            // q, r = divmod(n, base)
+	                            // num_buf[i] = r
+	                            // i += 1
+	                            // n = q
+	b baseN
+	
+base2:
+	cmp r9, #0
+	beq finish
+	and r4, r9, #0b1
+	add r2, #1
+	str r4, [r0, r2]
+	lsr r9, #1
+	b base2
+
+base4:
+	cmp r9, #0
+	beq finish
+	and r4, r9, #0b11
+	add r2, #1
+	str r4, [r0, r2]
+	lsr r9, #2
+	b base4
+
+base8:
+	cmp r9, #0
+	beq finish
+	and r4, r9, #0b111
+	add r2, #1
+	str r4, [r0, r2]
+	lsr r9, #3
+	b base8
+
+base16:
+	cmp r9, #0
+	beq finish
+	and r4, r9, #0b1111
+	add r2, #1
+	str r4, [r0, r2]
+	lsr r9, #4
+	b base16
+
+base32:
+	cmp r9, #0
+	beq finish
+	and r4, r9, #0b11111
+	add r2, #1
+	str r4, [r0, r2]
+	lsr r9, #5
+	b base32
+
+finish:                    
+	push {r2}              // push string length
+	cmp r2, #1             // only need to reverse the string if it's longer than 1 char
+	ble next
+	mov r1, #0             // r0 = index
+revloop:                   // Reverse the number buffer for printing
+	ldrb r3, [r0, r1]      // fetch at two indices
+	ldrb r4, [r0, r2]
+
+	cmp r3, #9             // convert digits to chars
+	addgt r3, #7           // if digit >9 then make A = digit.10
+	cmp r4, #9
+	addgt r4, #7
+	add r3, #'0'
+	add r4, #'0'
+
+	strb r3, [r0, r2]      // swap the chars into different indices
+	strb r4, [r0, r1]
+
+	sub r2, #1             // move indices towards the middle
+	add r1, #1
+
+	cmp r1, r2             // we're done when no more chars to swap
+	bne revloop
+	b next
+
