bf30f4d2dd442c525ef63503ba872abe5fa0dbc3
ihalseide <imitatio-ignotum@protonmail.com>
2021-04-12 16:14:59 -0600

    Fix u_dot.s

--- u_dot.s
+++ u_dot.s
@@ -1,9 +1,9 @@
 // FORTH word
-// u>chars ( u -- addr n )
-// where u is an unsigned number
-// where addr is the pad address, for the beginning address of the character string
-// where n is the length of the string of chars, n is 0 if error
-u_dot:
+// u>chars ( u1 -- addr u2 )
+// where u1 is an unsigned number;
+// where addr is the start address for the output string;
+// where u2 is the length of the output string, and is 0 upon an error.
+u_to_chars:
 	ldr r0, =var_pad   // r0 = pad addr
 	ldr r0, [r0]
 	push {r0}          // push addr
@@ -15,15 +15,15 @@ u_dot:
 	movlt r9, #0          
 	blt next
 
-	cmp r9, #0         // handle the trivial case of u = 0
+	cmp r9, #0         // u = 0 is a trivial case
 	moveq r1, #'0'
 	streq r1, [r0]
 	moveq r9, #1
-	beq next
+	beq next           // early return
 
 	mov r2, #-1        // r2 = index i
 
-    // Jump to extract digits with a base that is a power of 2
+    // Hard-coded routines for a base that is a power of 2
 	cmp r1, #2
 	beq base2
 	cmp r1, #4
@@ -36,45 +36,39 @@ u_dot:
 	beq base32
 
 baseN:                        // Base that is not a power of 2
-	cmp r9, #0
+	cmp r9, #0                // loop until input number is divided and turned into 0
 	beq finish
 
-	// (Inlined) Function for integer division modulo
-	// Copied from the project https://github.com/organix/pijFORTHos (which itself is a copy)
-	// Arguments: r0 = numerator, r1 = denominator
-	// Returns: r0 = remainder, r1 = denominator, r2 = quotient
-	push {r0-r2}
-	mov r0, r9
-
-	mov r3, r1
-	cmp r3, r0, LSR #1
-1:	movls r3, r3, LSL #1
-	cmp r3, r0, LSR #1
+	// This (Inlined) Function for integer division modulo
+	// Copied and modified from the project https://github.com/organix/pijFORTHos (which itself is a copy)
+	// Arguments: r3 = numerator, r1 = denominator
+	// Returns: r3 = remainder, r1 = denominator, r4 = quotient
+	// Temporary: r6
+	mov r6, r1
+	cmp r6, r3, LSR #1
+1:	movls r6, r6, LSL #1
+	cmp r6, r3, LSR #1
 	bls 1b
-	mov r2, #0
-2:	cmp r0, r3
-	subcs r0, r0, r3
-	adc r2, r2, r2
-	mov r3, r3, LSR #1
-	cmp r3, r1
+	mov r4, #0
+2:	cmp r3, r6
+	subcs r3, r3, r6
+	adc r4, r4, r4
+	mov r6, r6, LSR #1
+	cmp r6, r1
 	bhs 2b
 
-	mov r9, r2
-	mov r3, r0
-	pop {r0-r2}
-
-	add r2, #1
-	str r3, [r0, r2]
-
-	b baseN
+_div_end:	mov r9, r4         // n = n / base (quotient)
+	add r2, #1         // increment index into the number output buffer
+	str r3, [r0, r2]   // put remainder into the buffer
+	b baseN            // loop
 	
-base2:
+base2:                   // this program structure is duplicated for bases which are powers of 2
 	cmp r9, #0
 	beq finish
-	and r4, r9, #0b1
-	add r2, #1
-	str r4, [r0, r2]
-	lsr r9, #1
+	and r4, r9, #0b1     // get last bit(s)
+	add r2, #1           // increment index into number buffer
+	strb r4, [r0, r2]    // store into number buffer
+	lsr r9, #1           // shift the number down
 	b base2
 
 base4:
@@ -82,7 +76,7 @@ base4:
 	beq finish
 	and r4, r9, #0b11
 	add r2, #1
-	str r4, [r0, r2]
+	strb r4, [r0, r2]
 	lsr r9, #2
 	b base4
 
@@ -91,7 +85,7 @@ base8:
 	beq finish
 	and r4, r9, #0b111
 	add r2, #1
-	str r4, [r0, r2]
+	strb r4, [r0, r2]
 	lsr r9, #3
 	b base8
 
@@ -100,7 +94,7 @@ base16:
 	beq finish
 	and r4, r9, #0b1111
 	add r2, #1
-	str r4, [r0, r2]
+	strb r4, [r0, r2]
 	lsr r9, #4
 	b base16
 
@@ -109,12 +103,12 @@ base32:
 	beq finish
 	and r4, r9, #0b11111
 	add r2, #1
-	str r4, [r0, r2]
+	strb r4, [r0, r2]
 	lsr r9, #5
 	b base32
 
 finish:                    
-	push {r2}              // push string length
+	mov r9, r2             // push the string length to the stack
 	cmp r2, #1             // only need to reverse the string if it's longer than 1 char
 	ble next
 	mov r1, #0             // r0 = index
