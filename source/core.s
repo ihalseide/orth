aaa78af666fc0da1f5b405a138547689bcc16f15
ihalseide <imitatio-ignotum@protonmail.com>
2021-04-16 14:46:59 -0600

    Replace n>str with u>str

--- source/core.s
+++ source/core.s
@@ -470,141 +470,127 @@ to_num_done:                  // number conversion done
 	push {r1}                 // push the high word
 	NEXT
 
-defword "n>str", 5, u_to_str  // ( n1 -- addr u2 )
-	ldr r0, =var_h            // r0 = pad addr
-	ldr r0, [r0]
-	add r0, #64
-	push {r0}                 // push addr
-
-	ldr r1, =var_base         // r1 = number base
-	ldr r1, [r1]
-
-	cmp r1, #2                // if base < 2, error condition
-	movlt r9, #0                 
-	blt next
-
-	cmp r9, #0                // u = 0 is a trivial case
-	moveq r1, #'0'
-	streq r1, [r0]
-	moveq r9, #1
-	beq next                  // early return
-
-	eor r2, r2                // use the stack to remember if the number is negative
-	cmp r9, #0
-	addle r2, #1
-	push {r2}
-
-	mov r2, #-1               // r2 = index i
-
-    // Hard-coded routines for a base that is a power of 2
-	cmp r1, #2
+defcode "u>str", 5, u_to_str  // ( u1 -- addr u2 )
+	/* Get the pad address and make an index into it */
+	mov r4, #0                // r4 = index
+	ldr r5, =var_h          
+	ldr r5, [r5]              // r5 = pad
+	add r5, #30               // add some arbitrary padding
+	/* Get the number base */
+	ldr r6, =var_base         // r6 = number base
+	ldr	r6, [r6]
+	/* Only proceed if the number base is valid */
+	cmp	r6, #1
+	bgt	good_base
+	mov r9, 0                 // ( 0 0 )
+	push {r9}
+	NEXT
+good_base:
+	cmp	r9, #0
+	bne	u_not_zero
+	/* Write a 0 to the pad if u is 0 */
+	add r4, #1
+	eor r0, r0
+	str r0, [r5]
+u_not_zero:
+	/* Switch on the number base */
+	tst r6, #1     // the base can't be a power of 2 if it's odd
+	b base_other
+	cmp r6, #2
 	beq base2
-	cmp r1, #4
+	cmp r6, #4
 	beq base4
-	cmp r1, #8
+	cmp r6, #8
 	beq base8
-	cmp r1, #16
+	cmp r6, #16
 	beq base16
-	cmp r1, #32
-	beq base32
-
-baseN:                        // Base that is not a power of 2
-	cmp r9, #0                // loop until input number is divided and turned into 0
-	beq finish
-
-	// This (Inlined) Function for integer division modulo
-	// Copied and modified from the project https://github.com/organix/pijFORTHos (which itself is a copy)
-	// Arguments: r3 = numerator, r1 = denominator
-	// Returns: r3 = remainder, r1 = denominator, r4 = quotient
-	// Temporary: r6
-	mov r6, r1
-	cmp r6, r3, LSR #1
-1:	movls r6, r6, LSL #1
-	cmp r6, r3, LSR #1
-	bls 1b
-	mov r4, #0
-2:	cmp r3, r6
-	subcs r3, r3, r6
-	adc r4, r4, r4
-	mov r6, r6, LSR #1
-	cmp r6, r1
-	bhs 2b
-
-	mov r9, r4          // n = n / base (quotient)
-	add r2, #1          // increment index into the number output buffer
-	strb r3, [r0, r2]   // put remainder into the buffer
-	b baseN             // loop
-base2:                   // this program structure is duplicated for bases which are powers of 2
+	cmp r6, #32
+	beq base32     // default case
+	b base_other
+base2_body:
+	and r0, r9, #1
+	strb r0, [r5, r4]
+	add r4, #1
+	lsr r9, r9, #1
 	cmp r9, #0
-	beq finish
-	and r4, r9, #0b1     // get last bit(s)
-	add r2, #1           // increment index into number buffer
-	strb r4, [r0, r2]    // store into number buffer
-	lsr r9, #1           // shift the number down
-	b base2
+base2:
+	bne base2_body
+	b base_done
+base4_body:
+	and r0, r9, #3
+	strb r0, [r5, r4]
+	add r4, #1
+	lsr r9, r9, #2
+	cmp r9, #0
 base4:
+	bne base4_body
+	b base_done
+base8_body:
+	and r0, r9, #7
+	strb r0, [r5, r4]
+	add r4, #1
+	lsr r9, r9, #3
 	cmp r9, #0
-	beq finish
-	and r4, r9, #0b11
-	add r2, #1
-	strb r4, [r0, r2]
-	lsr r9, #2
-	b base4
 base8:
-	cmp r9, #0
-	beq finish
-	and r4, r9, #0b111
-	add r2, #1
-	strb r4, [r0, r2]
-	lsr r9, #3
-	b base8
+	bne base8_body
+	b base_done
+base16_body:
+	and r0, r9, #15
+	strb r0, [r5, r4]
+	add r4, #1
+	lsr r9, r9, #4
 base16:
 	cmp r9, #0
-	beq finish
-	and r4, r9, #0b1111
-	add r2, #1
-	strb r4, [r0, r2]
-	lsr r9, #4
-	b base16
+	bne base16_body
+	b base_done
+base32_body:
+	and r0, r9, #31
+	strb r0, [r5, r4]
+	add r4, #1
+	lsr r9, r9, #5
 base32:
 	cmp r9, #0
-	beq finish
-	and r4, r9, #0b11111
-	add r2, #1
-	strb r4, [r0, r2]
-	lsr r9, #5
-	b base32
-finish:                    
-	pop {r3}               // get if the number was negative
-	cmp r3, #0
-	movne r4, #'-'
-	add r2, r3
-	strbne r4, [r0, r2]
-
-	add r9, r2, #1         // move the string length to TOS
-	cmp r2, #1             // only need to reverse the string if it's longer than 1 char
-	blt next
-	mov r1, #0             // r0 = index
-revloop:                   // Reverse the number buffer for printing
-	ldrb r3, [r0, r1]      // fetch at two indices
-	ldrb r4, [r0, r2]
-
-	cmp r3, #9             // Make digits larger than 9 to map to ascii A
+	bne base32_body
+	b base_done
+base_other_body:
+	mov r0, r9         // numerator = u (TOS)
+	mov r1, r6         // denominator = base
+	bl fn_divmod
+	mov r3, r0         // u % base -> rem
+	mov r9, r2         // u / base -> u
+	strb r0, [r5, r4]  // rem -> pad[i]
+	add r4, #1
+base_other:
+	cmp r9, #0
+	bne base_body
+base_done:
+	/* Reverse the pad array */
+	mov r9, r4          // TOS = pad length
+	eor r0, r0          // r0 = pad index #1
+	mov r1, r1          // r1 = pad index #2
+	b reverse
+reverse_body:
+	/* Get the characters on the opposite sides of the array */
+	ldrb r2, [r5, r0]
+	ldrb r3, [r5, r1]
+	/* Convert values to digits */
+	cmp r2, #9
+	addgt r2, #7
+	cmp r3, #9
 	addgt r3, #7
-	cmp r4, #9
-	addgt r4, #7
-
-	add r3, #'0'           // Base off of the ascii char for 0
-	add r4, #'0'
-
-	strb r3, [r0, r2]      // swap the chars into different indices
-	strb r4, [r0, r1]
-
-	sub r2, #1             // move indices towards the middle
-	add r1, #1
-
-	cmp r1, r2             // we're done when no more chars to swap
-	blt revloop
+	add r2, #'0'
+	add r3, #'0'
+	/* Swap characters */
+	str r2, [r5, r1]
+	str r3, [r5, r0]
+	/* Move indices towards each other */
+	add r0, #1
+	sub r1, #1
+reverse:
+	cmp r1, r2
+	bls reverse_body
+	/* done, return */
+	push {r5}       // second item on stack is the pad start address
 	NEXT
 
 // ----- High-level words ----- 
